3742a717d8246ac8769241ada007a8afdd727951
viktor <gjiuhhbiu21@gmail.com>
2024-02-07 15:50:49 +0300

    added DB connection

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import com.zaxxer.hikari.HikariConfig;
 import io.javalin.Javalin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,16 +10,29 @@ public class App {
         Javalin app = getApp();
         app.start(getPort());
     }
-    public static Javalin getApp(){
+
+    public static Javalin getApp() {
         Logger logger = LoggerFactory.getLogger(App.class);
         var app = Javalin.create();
         app.before(ctx -> {
             ctx.contentType("text/html; charset=utf-8");
         });
+
+        //root
         app.get("/", ctx -> ctx.result("Hello World"));
 
+        //init DB
+        var hikariConfig = new HikariConfig();
+        if (System.getenv("DATABASE_URL") != null) {
+            hikariConfig.setUsername(System.getenv("DATABASE_USERNAME"));
+            hikariConfig.setPassword(System.getenv("DATABASE_PASSWORD"));
+            hikariConfig.setJdbcUrl(System.getenv("DATABASE_URL"));
+        } else {
+            hikariConfig.setJdbcUrl("jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+        }
         return app;
     }
+
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "7070");
         return Integer.valueOf(port);
